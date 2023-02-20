803ee427c1f38281f83ac63f6bc7f5ae4b9ea0be
Muhammad Burhan Ahmad <57236126+burhanahmad02@users.noreply.github.com>
2023-02-20 20:22:25 +0400

    Character Controller and input system

--- Assets/_Assets/Scripts/Player.cs
+++ Assets/_Assets/Scripts/Player.cs
@@ -3,32 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    private float moveSpeed = 7f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
+
 
     private bool isWalking;
     // Update is called once per frame
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.VectorInputNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir *moveSpeed * Time.deltaTime;
 
--- Assets/_Assets/Scripts/PlayerAnimator.cs
+++ Assets/_Assets/Scripts/PlayerAnimator.cs
@@ -8,7 +8,7 @@ public class PlayerAnimator : MonoBehaviour
     private Animator animator;
 
 
-    [SerializeField] private Player player;  
+    [SerializeField]     private Player player;  
     // Start is called before the first frame update
     private void Awake()
     {
