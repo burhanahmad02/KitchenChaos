b589ddfd044020c8bca3357d69cea0dc19175366
Muhammad Burhan Ahmad <57236126+burhanahmad02@users.noreply.github.com>
2023-02-20 06:52:04 +0400

    Added PlayerController

---
+++ Assets/_Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 7f;
+
+    private bool isWalking;
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+        inputVector = inputVector.normalized;
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir *moveSpeed * Time.deltaTime;
+
+        isWalking = moveDir != Vector3.zero;
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+
+    }
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+}
---
+++ Assets/_Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private const string IS_Walking = "IsWalking";
+    private Animator animator;
+
+
+    [SerializeField] private Player player;  
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        animator.SetBool(IS_Walking, player.IsWalking());
+    }
+}
